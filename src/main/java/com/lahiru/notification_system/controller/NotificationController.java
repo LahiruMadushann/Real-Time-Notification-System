f7059338b3d9b135fd1b0a0d41b07f971b18a736
LahiruMadushann <lahirumadushandl@gmail.com>
2025-09-28 14:24:56 +0530

    feat : implement controller

---
+++ src/main/java/com/lahiru/notification_system/controller/NotificationController.java
@@ -0,0 +1,15 @@
+package com.lahiru.notification_system.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notification")
+    public String sendMessage(String message) {
+        return message;
+    }
+}
